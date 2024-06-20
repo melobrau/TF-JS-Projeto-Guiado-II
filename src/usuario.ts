f7a21bdd32886d8d9bd2b132ce56086f3526a20f
Melo Mateus <100786778+melobrau@users.noreply.github.com>
2024-06-20 01:15:04 -0300

    Add files via upload

---
+++ src/usuario.ts
@@ -0,0 +1,10 @@
+class Usuario{
+    nome: string;
+    email: string;
+    senha: string;
+    constructor(nome: string, email: string, senha: string){
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+}
