191c45b246cfb6ab5f85a9855405a9dbaa6f9d86
Melo Mateus <100786778+melobrau@users.noreply.github.com>
2024-06-20 15:24:39 -0300

    aluno.ts
    classe Aluno

---
+++ aluno.ts
@@ -0,0 +1,8 @@
+class Aluno extends Usuario{
+    matricula: string;
+
+    constructor(nome:string, email:string, senha:string, matricula:string){
+        super(nome, email, senha);
+        this.matricula = matricula;
+    }
+}
